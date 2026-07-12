8f8c98b62596b892b08bf3f4984e9e75c6ef5d42
dipak-4321 <dipakprince786@gmail.com>
2026-07-12 12:42:52 +0545

    checkWinner function is added

--- scripts.js
+++ scripts.js
@@ -2,6 +2,17 @@ let boxbtn = document.querySelectorAll(".box");
 
 let turn0 = true;
 
+let winpatterns =[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
 boxbtn.forEach((box)=>{
 box.addEventListener("click", ()=>{
     if(turn0){
@@ -12,5 +23,21 @@ box.addEventListener("click", ()=>{
         turn0 = true;
     }
     box.disabled = true;
+
+    checkwinner();
 });
 });
+const checkwinner = () => {
+    for (let pattern of winpatterns){
+        let val1 = boxbtn[pattern[0]].innerText;
+        let val2 = boxbtn[pattern[1]].innerText;
+        let val3 = boxbtn[pattern[2]].innerText;
+    
+    if(val1 != "" && val2 != "" && val3 != ""){
+        if(val1 === val2 && val2 === val3){
+            console.log("winner",val1);
+        }
+    }
+    }
+
+};
